0e24f489f8aa783038db740552cf699f56491503
júlia <162610003+juliamacedo63@users.noreply.github.com>
2024-11-25 07:46:55 -0300

    Exercício 8

--- CAP03/EXERCÍCIOS-RESOLVIDOS/EXE08/src/br/edu/principal/Principal.java
+++ CAP03/EXERCÍCIOS-RESOLVIDOS/EXE08/src/br/edu/principal/Principal.java
@@ -1,5 +1,11 @@
-package br.edu.principal;
-
 public class Principal {
-
+    public static void main(String[] args){
+        double deposito = 13;
+        double juros = 5;
+        double rendimento = deposito * (juros / 100);
+        double valor_total = deposito + rendimento;
+        System.out.println("O valor do rendimento é " + rendimento);
+        System.out.println("O valor total é " + valor_total);
+    }
+    
 }
